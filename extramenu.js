8473f9065628eec8aa0f753bd3c03bc261b7f9af
Tecnologe <79880328+Tecnologe@users.noreply.github.com>
2024-07-08 00:18:37 +0100

    Add files via upload

---
+++ extramenu.js
@@ -0,0 +1,7 @@
+// script.js
+
+function toggleMenu() {
+    var menu = document.getElementById('menuDesplegable');
+    menu.style.display = menu.style.display === 'block' ? 'none' : 'block';
+ }
+ 
